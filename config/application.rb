c046efa453af6f8d68b4641717977b2a90532636
Cesar Madera Garces <cmadera@bancolombia.com.co>
2024-04-22 12:10:12 -0500

    deleted package lock

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Yllen
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.compile = true
+    config.assets.digest = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
